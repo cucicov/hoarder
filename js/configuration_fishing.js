265fb90ab2058902979aa317dec3eff1b8c80964
Maria Nastase <o.maria.nastase@gmail.com>
2020-06-02 00:52:57 +0300

    ok

--- js/configuration_fishing.js
+++ js/configuration_fishing.js
@@ -4,8 +4,8 @@ function getImagePositioningParams() {
             {
                 id: 'canvas1',
                 zIndex: -1,
-                facePositionTopOffset: 610,
-                facePositionLeftOffset: 1060,
+                facePositionTopOffset: 595,
+                facePositionLeftOffset: 1035,
                 ratio: 0.45,
                 filter: 'grayscale(0%) sepia(80%) hue-rotate(320deg)' ,
                 inverseMirror: false,
@@ -26,8 +26,8 @@ function getImagePositioningParams() {
             {
                 id: 'canvas2',
                 zIndex: -1,
-                facePositionTopOffset: 290,
-                facePositionLeftOffset: 945,
+                facePositionTopOffset: 275,
+                facePositionLeftOffset: 920,
                 ratio: 0.25,
                 filter: 'grayscale(0%) sepia(0%)',
                 inverseMirror: false,
@@ -46,8 +46,8 @@ function getImagePositioningParams() {
             {
                 id: 'canvas3',
                 zIndex: -2,
-                facePositionTopOffset: 255,
-                facePositionLeftOffset: 1235,
+                facePositionTopOffset: 230,
+                facePositionLeftOffset: 1220,
                 ratio: 0.25,
                 filter: 'grayscale(0%) sepia(0%)',
                 inverseMirror: true,
@@ -66,8 +66,8 @@ function getImagePositioningParams() {
             {
                 id: 'canvas4',
                 zIndex: -3,
-                facePositionTopOffset: 732,
-                facePositionLeftOffset: 1000,
+                facePositionTopOffset: 705,
+                facePositionLeftOffset: 975,
                 ratio: 0.25,
                 filter: 'grayscale(0%) sepia(10%)',
                 inverseMirror: false,
@@ -86,8 +86,8 @@ function getImagePositioningParams() {
             {
                 id: 'canvas5',
                 zIndex: -4,
-                facePositionTopOffset: 725,
-                facePositionLeftOffset: 1125,
+                facePositionTopOffset: 695,
+                facePositionLeftOffset: 1110,
                 ratio: 0.25,
                 filter: 'grayscale(0%) sepia(0%)',
                 inverseMirror: true,
@@ -108,7 +108,7 @@ function getImagePositioningParams() {
             {
                 id: 'canvas6',
                 zIndex: -1,
-                facePositionTopOffset: 655,
+                facePositionTopOffset: 620,
                 facePositionLeftOffset: 490,
                 ratio: 0.22,
                 filter: 'grayscale(0%) sepia(0%) ',
